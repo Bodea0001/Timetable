b8dfa0c32f501788dadd83800c999ea7f34bdbef
Bogdan Objilean <bodea0001@gmail.com>
2022-12-07 12:43:58 +0300

    fix user.py

--- controllers/user.py
+++ controllers/user.py
@@ -1,15 +1,17 @@
 import jwt
 from fastapi import Depends, HTTPException, status
+from sqlalchemy.orm import Session
 
-from controllers.crud import get_user
+from sql.crud import get_user
 from controllers.password import verify_password
 from controllers.oauth2 import oauth2_scheme
+from controllers.db import get_db
 from models.schemas import TokenData
 from config import SECRET_KEY, ALGORITHM
 
 
-def authenticate_user(username: str, password: str):
-    user = get_user(username)
+def authenticate_user(db: Session, username: str, password: str):
+    user = get_user(db, username)
     if not user:
         return False
     if not verify_password(password, user.password):
@@ -17,7 +19,7 @@ def authenticate_user(username: str, password: str):
     return user
 
 
-async def get_current_user(token: str = Depends(oauth2_scheme)):
+async def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -31,7 +33,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         token_data = TokenData(username=username)
     except jwt.PyJWTError:
         raise credentials_exception
-    user = get_user(username=token_data.username)
+    user = get_user(db, username=token_data.username)
     if user is None:
         raise credentials_exception
     return user

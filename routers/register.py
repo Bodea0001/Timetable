8ff0dc70a0c4b132434cece5074a6f638fcac26e
Bogdan Objilean <bodea0001@gmail.com>
2022-12-07 15:42:52 +0300

    update register.py

--- routers/register.py
+++ routers/register.py
@@ -1,20 +1,21 @@
-from fastapi import APIRouter, Depends, status, HTTPException
+from datetime import timedelta
 from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, status, HTTPException
 
+from sql import models
+from sql.crud import create_user
 from models import schemas
 from controllers.db import get_db
 from controllers.user import get_user
-from controllers.token import get_token
 from controllers.email import is_email_valid
+from controllers.token import create_access_token
 from controllers.password import get_password_hash
-from sql.crud import create_user
-from sql import models
-
+from config import ACCESS_TOKEN_EXPIRE_MINUTES
 
 router = APIRouter()
 
 
-@router.post("/register", response_model=schemas.UserOut ,tags=["register"], status_code=status.HTTP_201_CREATED)
+@router.post("/register", response_model=schemas.Token ,tags=["register"], status_code=status.HTTP_201_CREATED)
 async def process_register(form_data: schemas.OAuth2PasswordRequestFormUpdate = Depends(), db: Session = Depends(get_db)):
     email = form_data.username
     if not is_email_valid(email):
@@ -37,9 +38,11 @@ async def process_register(form_data: schemas.OAuth2PasswordRequestFormUpdate = 
         last_name=form_data.last_name.capitalize(),
     )
     db_user = create_user(db, user)
-    return schemas.UserOut(
-        id=db_user.id, # type: ignore
-        email=db_user.email, # type: ignore
-        first_name=db_user.first_name, # type: ignore
-        last_name=db_user.last_name, # type: ignore
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={
+            "sub": db_user.email,
+            },
+        expires_delta=access_token_expires
     )
+    return {"access_token": access_token, "token_type": "bearer"}

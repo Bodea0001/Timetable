00d28cb6a15ed199e57a835ee161974b4e726e7a
Bogdan Objilean <bodea0001@gmail.com>
2022-12-07 12:33:54 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="timetable",
+    version="1.0.0",
+    url="https://github.com/Bodea0001/Timetable",
+    packages=find_packages()
+)

5e766191f7fd4cbf01a735a66a356d14fc7f4001
OneFallDen <onefallden@gmail.com>
2022-12-11 19:00:01 +0300

    task: added router to delete tasks

--- routers/task_router.py
+++ routers/task_router.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from models import schemas
 from controllers.user import get_current_user
 from controllers.db import get_db
-from controllers.task_controller import addTask, getTaskBySubject
+from controllers.task_controller import addTask, getTaskBySubject, getAllTaskInTable, deleteTaskFromTable
 
 router = APIRouter()
 
@@ -22,4 +22,9 @@ async def get_task_by_subject(subject: str, id_timetable: int, db: Session = Dep
 
 @router.get('/task', tags=['task'], status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_user)])
 async def get_task(id_timetable: int, db: Session = Depends(get_db)):
-    return 1
+    return getAllTaskInTable(id_timetable, db)
+
+
+@router.delete('/task', tags=['task'], status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_user)])
+async def delete_task(id_timetable: int, id_task: int, db: Session = Depends(get_db)):
+    return deleteTaskFromTable(id_timetable, id_task, db)

22e0216f037849d2c49f5848f4a552bf4d35726d
OneFallDen <onefallden@gmail.com>
2022-12-11 19:00:21 +0300

    task: added def to delete tasks

--- controllers/task_controller.py
+++ controllers/task_controller.py
@@ -2,7 +2,7 @@ from fastapi import Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from models import schemas
-from sql.crud import create_task, get_task_by_subject, get_all_tasks_in_table
+from sql.crud import create_task, get_task_by_subject, get_all_tasks_in_table, delete_task_from_table
 
 
 def addTask(task: schemas.TaskBase, db: Session):
@@ -15,4 +15,9 @@ def getTaskBySubject(subject: str, id_timetable: int, db: Session):
 
 
 def getAllTaskInTable(id_timetable: int, db: Session):
-    get_all_tasks_in_table(db, id_timetable)
+    return get_all_tasks_in_table(db, id_timetable)
+
+
+def deleteTaskFromTable(id_timetable: int, id_task: int, db: Session):
+    delete_task_from_table(db, id_timetable, id_task)
+    return HTTPException(status_code=200, detail='Task deleted successfully')

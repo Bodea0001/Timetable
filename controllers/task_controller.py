5950638e90ce962970a3802eb814d0077f1e112a
OneFallDen <onefallden@gmail.com>
2022-12-11 16:41:14 +0300

    task: added task def to get tasks by subject

--- controllers/task_controller.py
+++ controllers/task_controller.py
@@ -2,9 +2,13 @@ from fastapi import Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from models import schemas
-from sql.crud import create_task
+from sql.crud import create_task, get_task_by_subject
 
 
 def addTask(task: schemas.TaskBase, db: Session):
     create_task(db, task)
     return HTTPException(status_code=201, detail='Task created successfully')
+
+
+def getTaskBySubject(subject: str, id_timetable: int, db: Session):
+    return get_task_by_subject(db, id_timetable, subject)

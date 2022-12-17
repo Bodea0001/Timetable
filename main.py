491a6abee8007fe624ce471280ad883535f14872
Bogdan Objilean <bodea0001@gmail.com>
2022-12-17 11:29:38 +0300

    implement router 'week_router.py'

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from routers import login, register, user_info, user_token, task_router, timetable_router
+from routers import login, register, user_info, user_token, task_router, timetable_router, week_router
 
 
 app = FastAPI()
@@ -11,4 +11,5 @@ app.include_router(register.router)
 app.include_router(user_info.router)
 app.include_router(user_token.router)
 app.include_router(task_router.router)
+app.include_router(week_router.router)
 app.include_router(timetable_router.router)

c6a6fd0092b32d9826628d3b4f402e2bc37c1849
OneFallDen <onefallden@gmail.com>
2022-12-18 22:46:52 +0300

    task: changes in router to create task with statuses

--- routers/task_router.py
+++ routers/task_router.py
@@ -11,27 +11,28 @@ router = APIRouter()
 
 
 # Add new  task
-@router.post("/task", tags=["task"], response_model=schemas.TaskBase, status_code=status.HTTP_201_CREATED,
-             dependencies=[Depends(get_current_user)])
-async def create_task(task: schemas.TaskBase, db: Session = Depends(get_db)):
-    return addTask(task, db)
+@router.post("/task", tags=["task"], status_code=status.HTTP_201_CREATED, dependencies=[Depends(get_current_user)])
+async def create_task(task: schemas.TaskOut, db: Session = Depends(get_db),
+                      user: schemas.User = Depends(get_current_user)):
+    return addTask(task, db, user.id)
 
 
 # Get tasks by subject in timetable
-@router.get('/task/subject', tags=['task'], response_model=schemas.TaskBase, status_code=status.HTTP_200_OK,
+@router.get('/task/subject', tags=['task'], status_code=status.HTTP_200_OK,
             dependencies=[Depends(get_current_user)])
 async def get_task_by_subject(subject: str, id_timetable: int, db: Session = Depends(get_db)):
     return getTaskBySubject(subject, id_timetable, db)
 
 
 # Get all users tasks
-@router.get('/task/user_id', tags=['task'], status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_user)])
+@router.get('/task/user_id', tags=['task'], status_code=status.HTTP_200_OK,
+            dependencies=[Depends(get_current_user)])
 async def get_task_by_user_id(db: Session = Depends(get_db), user: schemas.User = Depends(get_current_user)):
     return get_task_by_userid(db, user.id)
 
 
 # Get tasks in timetable by timetable id
-@router.get('/task', tags=['task'], response_model=schemas.TaskBase, status_code=status.HTTP_200_OK,
+@router.get('/task', tags=['task'], status_code=status.HTTP_200_OK,
             dependencies=[Depends(get_current_user)])
 async def get_task(id_timetable: int, db: Session = Depends(get_db)):
     return getAllTaskInTable(id_timetable, db)

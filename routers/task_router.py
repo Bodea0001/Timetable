ba9140d17ebaf43852140dbc9da9bf1f9fd04374
OneFallDen <onefallden@gmail.com>
2022-12-18 20:23:44 +0300

    task: added response models and comments

--- routers/task_router.py
+++ routers/task_router.py
@@ -11,26 +11,33 @@ router = APIRouter()
 
 
 # Add new  task
-@router.post("/task", tags=["task"], status_code=status.HTTP_201_CREATED, dependencies=[Depends(get_current_user)])
+@router.post("/task", tags=["task"], response_model=schemas.TaskBase, status_code=status.HTTP_201_CREATED,
+             dependencies=[Depends(get_current_user)])
 async def create_task(task: schemas.TaskBase, db: Session = Depends(get_db)):
     return addTask(task, db)
 
 
-@router.get('/task/subject', tags=['task'], status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_user)])
+# Get tasks by subject in timetable
+@router.get('/task/subject', tags=['task'], response_model=schemas.TaskBase, status_code=status.HTTP_200_OK,
+            dependencies=[Depends(get_current_user)])
 async def get_task_by_subject(subject: str, id_timetable: int, db: Session = Depends(get_db)):
     return getTaskBySubject(subject, id_timetable, db)
 
 
+# Get all users tasks
 @router.get('/task/user_id', tags=['task'], status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_user)])
 async def get_task_by_user_id(db: Session = Depends(get_db), user: schemas.User = Depends(get_current_user)):
     return get_task_by_userid(db, user.id)
 
 
-@router.get('/task', tags=['task'], status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_user)])
+# Get tasks in timetable by timetable id
+@router.get('/task', tags=['task'], response_model=schemas.TaskBase, status_code=status.HTTP_200_OK,
+            dependencies=[Depends(get_current_user)])
 async def get_task(id_timetable: int, db: Session = Depends(get_db)):
     return getAllTaskInTable(id_timetable, db)
 
 
+# Delete task by id
 @router.delete('/task', tags=['task'], status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_user)])
 async def delete_task(id_timetable: int, id_task: int, db: Session = Depends(get_db)):
     return deleteTaskFromTable(id_timetable, id_task, db)

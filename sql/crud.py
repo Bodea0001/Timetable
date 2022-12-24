7511259facc606e025771f122dfca41bbac0422c
Bogdan Objilean <bodea0001@gmail.com>
2022-12-24 13:29:00 +0300

    add statements with refresh token and white ip

--- sql/crud.py
+++ sql/crud.py
@@ -23,6 +23,41 @@ def create_user(db: Session, user: schemas.UserCreate) -> models.User:
     return db_user
 
 
+def create_user_refresh_token(db: Session, user_id: int | Column[Integer], refresh_token: str | Column[String]) -> models.UserRefreshToken:
+    db_user_refresh_token = models.UserRefreshToken(
+        id_user = user_id,
+        refresh_token = refresh_token
+    )
+    db.add(db_user_refresh_token)
+    db.commit()
+    db.refresh(db_user_refresh_token)
+    return db_user_refresh_token
+
+
+def delete_user_refresh_token(db: Session, user_id: int | Column[Integer], refresh_token: str | Column[String]):
+    db.query(models.UserRefreshToken).filter(
+        models.UserRefreshToken.id_user == user_id,
+        models.UserRefreshToken.refresh_token == refresh_token
+        ).delete()
+    db.commit()
+
+
+def delete_all_user_refresh_token(db: Session, user_id: int | Column[Integer]):
+    db.query(models.UserRefreshToken).filter(models.UserRefreshToken.id_user == user_id).delete()
+    db.commit()
+
+
+def create_user_white_ip(db: Session, user_id: int | Column[Integer], white_ip: str | Column[String]) -> models.UserWhiteIP:
+    db_user_white_ip = models.UserWhiteIP(
+        id_user = user_id,
+        white_ip = white_ip
+    )
+    db.add(db_user_white_ip)
+    db.commit()
+    db.refresh(db_user_white_ip)
+    return db_user_white_ip
+
+
 def get_tasks_by_user_id(db: Session, user_id: int):
     result = db.execute(select(models.TimetableUser).where(models.TimetableUser.id_user == user_id))
     tables = []

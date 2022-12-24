fda5b446cc9bbb369b67275c33f84563b0e2ce98
Bogdan Objilean <bodea0001@gmail.com>
2022-12-24 13:29:38 +0300

    add refresh token

--- routers/login.py
+++ routers/login.py
@@ -1,20 +1,25 @@
 from datetime import timedelta
-from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi import APIRouter, HTTPException, status, Depends, Request
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from models.schemas import Token
 from controllers.user import authenticate_user
-from controllers.token import create_access_token
+from controllers.token import create_access_token, create_refresh_token
 from controllers.db import get_db
-from config import ACCESS_TOKEN_EXPIRE_MINUTES
+from sql.crud import create_user_refresh_token, create_user_white_ip, delete_all_user_refresh_token
+from config import ACCESS_TOKEN_EXPIRE_MINUTES, REFRESH_TOKEN_EXPIRE_DAYS
 
 
 router = APIRouter()
 
 
 @router.post("/login", tags=["auth"], response_model=Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+async def login_for_access_token(
+    request: Request,
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db),
+    ):
     user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
@@ -22,11 +27,23 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
+
+    user_refresh_tokens = [value.refresh_token for value in user.refresh_tokens]  # type: ignore
+    if len(user_refresh_tokens) > 10:
+        delete_all_user_refresh_token(db, user.id)
+
+    token_data = {"sub": user.email}
+
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={
-            "sub": user.email,
-            },
-        expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
+    access_token = create_access_token(token_data, expires_delta=access_token_expires)
+
+    refresh_token_expires = timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    refresh_token = create_refresh_token(token_data, expires_delta=refresh_token_expires)
+    create_user_refresh_token(db, user.id, refresh_token)
+
+    white_ip = request.client.host  # type: ignore
+    user_white_list_ip = [value.white_ip for value in user.white_list_ip]  # type: ignore
+    if white_ip not in user_white_list_ip:
+        create_user_white_ip(db, user.id, white_ip)
+    
+    return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
--- routers/register.py
+++ routers/register.py
@@ -1,22 +1,26 @@
 from datetime import timedelta
 from sqlalchemy.orm import Session
-from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi import APIRouter, Depends, status, HTTPException, Request
 
 from sql import models
-from sql.crud import create_user
+from sql.crud import create_user, create_user_refresh_token, create_user_white_ip
 from models import schemas
 from controllers.db import get_db
 from controllers.user import get_user
 from controllers.email import is_email_valid
-from controllers.token import create_access_token
+from controllers.token import create_access_token, create_refresh_token
 from controllers.password import get_password_hash
-from config import ACCESS_TOKEN_EXPIRE_MINUTES
+from config import ACCESS_TOKEN_EXPIRE_MINUTES, REFRESH_TOKEN_EXPIRE_DAYS
 
 router = APIRouter()
 
 
 @router.post("/register", response_model=schemas.Token, tags=["auth"], status_code=status.HTTP_201_CREATED)
-async def process_register(form_data: schemas.OAuth2PasswordRequestFormUpdate = Depends(), db: Session = Depends(get_db)):
+async def process_register(
+    request: Request,
+    form_data: schemas.OAuth2PasswordRequestFormUpdate = Depends(),
+    db: Session = Depends(get_db)
+    ):
     email = form_data.username
     if not is_email_valid(email):
         raise HTTPException(
@@ -38,11 +42,17 @@ async def process_register(form_data: schemas.OAuth2PasswordRequestFormUpdate = 
         last_name=form_data.last_name.capitalize(),
     )
     db_user = create_user(db, user)
+
+    token_data = {"sub": db_user.email}
+
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={
-            "sub": db_user.email,
-            },
-        expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
+    access_token = create_access_token(token_data, expires_delta=access_token_expires)
+
+    refresh_token_expires = timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    refresh_token = create_refresh_token(token_data, expires_delta=refresh_token_expires)
+    create_user_refresh_token(db, db_user.id, refresh_token)
+
+    white_ip = request.client.host  # type: ignore
+    create_user_white_ip(db, db_user.id, white_ip)
+    
+    return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}

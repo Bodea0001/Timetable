522089cb8fa8dfcadfc5e553321db989f4cb8091
Bogdan Objilean <bodea0001@gmail.com>
2023-01-05 15:50:44 +0300

    add applications

--- routers/user_info.py
+++ routers/user_info.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from sql import models
 from models import schemas
 from controllers.db import get_db
-from controllers.user import get_current_user
+from controllers.user import get_current_user, validate_application
 from controllers.timetable import get_current_timetable, get_valid_timetable
 
 router = APIRouter()
@@ -12,19 +12,20 @@ router = APIRouter()
 
 @router.get(
     path="/users/me",
-    response_model=schemas.UserOutLite,
+    tags=["user"],
+    response_model=schemas.UserOut,
     summary="Get information about user",
     description="Get information about user by access token",
-    tags=["user", "timetable"]
     )
 async def read_user(db: Session = Depends(get_db), user: models.User = Depends(get_current_user)):
-    db_timetables = user.timetables_info
-    timetables = [get_valid_timetable(db, db_timetable) for db_timetable in db_timetables]  # type: ignore
-    return schemas.UserOutLite(
+    timetables = [get_valid_timetable(db, db_timetable) for db_timetable in user.timetables_info]  # type: ignore
+    applications = [validate_application(application) for application in user.applications]  # type: ignore
+    return schemas.UserOut(
         id=user.id, # type: ignore
         email=user.email, # type: ignore 
         first_name=user.first_name, # type: ignore
         last_name=user.last_name, # type: ignore
+        applications=applications,  # type: ignore
         timetables_info=timetables  # type: ignore
     )
     

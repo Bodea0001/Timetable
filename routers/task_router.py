7320314fa6bd73daae9b519c767e0b90d190d578
OneFallDen <onefallden@gmail.com>
2023-01-10 21:17:13 +0300

    fix comments

--- routers/task_router.py
+++ routers/task_router.py
@@ -10,7 +10,7 @@ from controllers.task_controller import addTask, getTaskBySubject, getAllTaskInT
 router = APIRouter()
 
 
-# Add new  task
+# Add new task
 @router.post("/task", tags=["task"], status_code=status.HTTP_201_CREATED, dependencies=[Depends(get_current_user)])
 async def create_task(task: schemas.TaskOut, db: Session = Depends(get_db),
                       user: schemas.User = Depends(get_current_user)):
@@ -24,21 +24,21 @@ async def get_task_by_subject(subject: str, id_timetable: int, db: Session = Dep
     return getTaskBySubject(subject, id_timetable, db)
 
 
-# Get all users tasks
+# Get all users tasks in all timetables
 @router.get('/task/user_id', tags=['task'], status_code=status.HTTP_200_OK,
             dependencies=[Depends(get_current_user)])
 async def get_task_by_user_id(db: Session = Depends(get_db), user: schemas.User = Depends(get_current_user)):
     return get_task_by_userid(db, user.id)
 
 
-# Get tasks in timetable by timetable id
-@router.get('/task', tags=['task'], status_code=status.HTTP_200_OK,
+# Get all users tasks in timetable
+@router.get('/task/single', tags=['task'], status_code=status.HTTP_200_OK,
             dependencies=[Depends(get_current_user)])
 async def get_task(id_timetable: int, db: Session = Depends(get_db), user: schemas.User = Depends(get_current_user)):
     return getAllTaskInTable(id_timetable, db, user.id)
 
 
-# Delete task by id
-@router.delete('/task', tags=['task'], status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_user)])
+# Delete task from all users in timetable
+@router.delete('/task/all', tags=['task'], status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_user)])
 async def delete_task(id_timetable: int, id_task: int, db: Session = Depends(get_db)):
     return deleteTaskFromTable(id_timetable, id_task, db)

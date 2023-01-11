46b25ea134f9da0919c14e01791f43725be9ce98
Bogdan Objilean <bodea0001@gmail.com>
2023-01-11 10:24:30 +0300

    add getting, updating and creating tasks

--- routers/task_router.py
+++ routers/task_router.py
@@ -1,36 +1,251 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from datetime import datetime, date
 
+from sql import models
+from sql.crud import (
+    get_task_by_id,
+    update_task_info,
+    get_tasks_by_date,
+    get_timetable_users_id,
+    update_task_user_status,
+    create_task_for_one_user,
+    get_timetable_user_status,
+    create_task_for_all_users,
+    get_user_task_relation_by_task_id,
+    get_timetable_user_relation_by_user_id_and_timetable_id,
+)
 from models import schemas
 from controllers.user import get_current_user
 from controllers.db import get_db
-from controllers.task_controller import addTask, getTaskBySubject, getAllTaskInTable, deleteTaskFromTable, \
-    get_task_by_userid
+from controllers.task_controller import (
+    addTask,
+    getTaskBySubject,
+    getAllTaskInTable,
+    deleteTaskFromTable, 
+    get_task_by_userid,
+    validate_task_for_user,
+    validate_task_for_elder,
+)
+from controllers.timetable import check_timetable
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/task",
+    tags=["task"]
+)
 
 
 # Add new  task
-@router.post("/task", tags=["task"], status_code=status.HTTP_201_CREATED, dependencies=[Depends(get_current_user)])
+@router.post("/", status_code=status.HTTP_201_CREATED, dependencies=[Depends(get_current_user)])
 async def create_task(task: schemas.TaskBase, db: Session = Depends(get_db)):
     return addTask(task, db)
 
 
-@router.get('/task/subject', tags=['task'], status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_user)])
+@router.get('/subject', status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_user)])
 async def get_task_by_subject(subject: str, id_timetable: int, db: Session = Depends(get_db)):
     return getTaskBySubject(subject, id_timetable, db)
 
 
-@router.get('/task/user_id', tags=['task'], status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_user)])
+@router.get('/user_id', status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_user)])
 async def get_task_by_user_id(db: Session = Depends(get_db), user: schemas.User = Depends(get_current_user)):
     return get_task_by_userid(db, user.id)
 
 
-@router.get('/task', tags=['task'], status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_user)])
+@router.get('/', status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_user)])
 async def get_task(id_timetable: int, db: Session = Depends(get_db)):
     return getAllTaskInTable(id_timetable, db)
 
 
-@router.delete('/task', tags=['task'], status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_user)])
+@router.delete('/', status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_user)])
 async def delete_task(id_timetable: int, id_task: int, db: Session = Depends(get_db)):
     return deleteTaskFromTable(id_timetable, id_task, db)
+
+# ---------------------------------------------
+
+@router.post(
+    path="/create_for_user",
+    status_code=status.HTTP_201_CREATED,
+    summary="Create a task for one user in the timetable"
+)
+async def create_task_for_user(
+    user_id: int,
+    task: schemas.TaskBase,
+    db: Session = Depends(get_db),
+    user: models.User = Depends(get_current_user)
+):
+    check_timetable(user, task.id_timetable)
+
+    timetable_current_user_status = get_timetable_user_status(db, user.id, task.id_timetable)
+    if (timetable_current_user_status != schemas.TimetableUserStatuses.elder or user_id != user.id):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=f"The current user doesn't have access rights to create the task to transmitted user in this timetable"
+        )
+    
+    timetable_user_relation = get_timetable_user_relation_by_user_id_and_timetable_id(db, user_id, task.id_timetable)
+    if not timetable_user_relation:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Unknow user"
+        )
+    create_task_for_one_user(db, user_id, task)
+
+
+@router.post(
+    path="/create_for_all",
+    status_code=status.HTTP_201_CREATED,
+    summary="Create a task for all users in the timetable"
+)
+async def create_task_for_all(
+    task: schemas.TaskBase,
+    db: Session = Depends(get_db),
+    user: models.User = Depends(get_current_user)
+):
+    check_timetable(user, task.id_timetable)
+
+    timetable_user_status = get_timetable_user_status(db, user.id, task.id_timetable)
+    if (timetable_user_status != schemas.TimetableUserStatuses.elder):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=f"The current user doesn't have access rights to create the task in this timetable"
+        )
+    
+    users_id = get_timetable_users_id(db, task.id_timetable)   
+    create_task_for_all_users(db, users_id, task)
+
+
+@router.get(
+    path="/get_in_timetable/for_user",
+    response_model=list[schemas.TaskOutForUser],
+    status_code=status.HTTP_200_OK,
+    summary="Get user's tasks fin the timetable"
+)
+async def get_timetable_tasks_for_elder(
+    timetable_id: int,
+    user: models.User = Depends(get_current_user)
+):
+    timetable = check_timetable(user, timetable_id)
+    user_timetable_tasks = [validate_task_for_user(task, user.id) for task in timetable.tasks]  # type: ignore
+    return [task for task in user_timetable_tasks if task]
+
+
+@router.get(
+    path="/get_in_timetable/for_elder",
+    response_model=list[schemas.TaskOutForElder],
+    status_code=status.HTTP_200_OK,
+    summary="Get timetable's tasks for elder"
+)
+async def get_user_tasks_in_timetable(
+    timetable_id: int,
+    db: Session = Depends(get_db),
+    user: models.User = Depends(get_current_user)
+):
+    timetable = check_timetable(user, timetable_id)
+
+    timetable_user_status = get_timetable_user_status(db, user.id, timetable_id)
+    if (timetable_user_status != schemas.TimetableUserStatuses.elder):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=f"The current user doesn't have access rights to get all tasks in this timetable"
+        )
+
+    elder_timetable_tasks = [validate_task_for_elder(db, task) for task in timetable.tasks]  # type: ignore 
+    return [task for task in elder_timetable_tasks if task]
+
+
+@router.get(
+    path="/get_all",
+    response_model=list[schemas.TaskOutForUser],
+    status_code=status.HTTP_200_OK,
+    summary="Get all user's tasks"
+)
+async def get_all_user_tasks(user: models.User = Depends(get_current_user)):
+    user_tasks = [validate_task_for_user(task, user.id) for timetable in user.timetables_info for task in timetable.tasks]  # type: ignore
+    return [task for task in user_tasks if task]
+
+
+@router.get(
+    path="/filter",
+    response_model=list[schemas.TaskOutForUser],
+    status_code=status.HTTP_200_OK,
+    summary="Filter tasks in the timetable by date and subject"
+)
+async def filter_user_tasks_by_date_and_subject(
+    timetable_id: int,
+    tasks_date: date,
+    db: Session = Depends(get_db),
+    user: models.User = Depends(get_current_user)
+):
+    check_timetable(user, timetable_id)
+
+    tasks = get_tasks_by_date(db, timetable_id, tasks_date, user.id)
+    if tasks:
+        return [validate_task_for_user(task, user.id) for task in tasks]
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="No tasks for the user in this timetable on the transmitted date"
+        )
+
+
+@router.patch(
+    path="/update/status",
+    status_code=status.HTTP_200_OK,
+    summary="Update user's status in the task"
+)
+async def update_user_status_in_task(
+    task_id: int,
+    task_status: schemas.TaskStatusesEnum,
+    db: Session = Depends(get_db),
+    user: models.User = Depends(get_current_user)
+):
+    user_task_relation = get_user_task_relation_by_task_id(db, task_id, user.id)
+    if not user_task_relation:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="The user doesn't have this task"
+        )
+    
+    update_task_user_status(db, task_id, user.id, task_status)
+
+
+@router.patch(
+    path="/update/info",
+    status_code=status.HTTP_200_OK,
+    summary="Update task's information"
+)
+async def update_task_information(
+    task_data: schemas.TaskUpdate,
+    db: Session = Depends(get_db),
+    user: models.User = Depends(get_current_user),
+):
+    task = get_task_by_id(db, task_data.id)
+    if not task:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Unknown task"
+        )
+    elif task.id_timetable != task_data.id_timetable:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="The transmitted timetable doesn't have this task"
+        )
+
+    user_task_relation = get_user_task_relation_by_task_id(db, task.id, user.id)
+    if not user_task_relation:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="The user doesn't have this task"
+        )
+    
+    if task.tag == schemas.TaskTags.all:
+        check_timetable(user, task.id_timetable)  # type: ignore
+
+        timetable_user_status = get_timetable_user_status(db, user.id, task.id_timetable)  # type: ignore
+        if (timetable_user_status != schemas.TimetableUserStatuses.elder):
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail=f"The current user doesn't have access rights to update this task"
+            )
+    
+    update_task_info(db, task_data.id, task_data.description, task_data.subject, task_data.deadline)

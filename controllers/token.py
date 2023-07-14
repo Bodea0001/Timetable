cc2f9efdccafe50fc7ebf752a60fcef18f7f6ff2
Bogdan Objilean <bodea0001@gmail.com>
2023-07-14 18:24:41 +0300

    add docstrings

--- controllers/token.py
+++ controllers/token.py
@@ -11,6 +11,7 @@ from models.schemas import AccessToken, RefreshToken, Token
 
 
 def create_token(data: dict, expires_delta: timedelta | None = None):
+    "Создаёт и возвращает токен"
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
@@ -22,11 +23,13 @@ def create_token(data: dict, expires_delta: timedelta | None = None):
 
 
 def _get_token_expire_stamp(expires_delta: timedelta) -> int:
+    "Возвращает POSIX-время, когда срок действия токена истечёт"
     token_time = datetime.utcnow() + expires_delta
     return int(datetime.timestamp(token_time))
 
 
 def _create_access_token(data: dict, expires_delta: int):
+    "Создает и возвращает access token и его срок действия"
     access_token_timedelta = timedelta(minutes=expires_delta)
     return AccessToken(
         access_token=create_token(data, access_token_timedelta),
@@ -35,6 +38,7 @@ def _create_access_token(data: dict, expires_delta: int):
 
 
 def _create_refresh_token(data: dict, expires_delta: int):
+    "Создает и возвращает refresh token и его срок действия"
     refresh_token_timedelta = timedelta(days=expires_delta)
     return RefreshToken(
         refresh_token=create_token(data, refresh_token_timedelta),
@@ -46,6 +50,7 @@ def create_access_and_refresh_tokens(
     access_expires_delta: int = ACCESS_TOKEN_EXPIRE_MINUTES,
     refresh_expires_delta: int = REFRESH_TOKEN_EXPIRE_DAYS
 ) -> Token:
+    "Создает и вовращает access и refresh токены, их сроки действия и тип токенов"
     access_token = _create_access_token(data, access_expires_delta)
     refresh_token = _create_refresh_token(data, refresh_expires_delta)
 

3bf0a55b5698b43ab15cb49f40c06bf264275425
Bogdan Objilean <bodea0001@gmail.com>
2023-07-19 11:36:36 +0300

    delete reading token router

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from routers import (
     login,
     register,
     user_info,
-    user_token,
     task_router,
     week_router,
     logout_router,
@@ -21,7 +20,6 @@ app = FastAPI()
 app.include_router(login.router)
 app.include_router(register.router)
 app.include_router(user_info.router)
-app.include_router(user_token.router)
 app.include_router(task_router.router)
 app.include_router(week_router.router)
 app.include_router(logout_router.router)

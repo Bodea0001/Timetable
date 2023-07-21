dcef51de5c5737d9630ad6fbb617271468a01662
Bogdan Objilean <bodea0001@gmail.com>
2023-07-21 16:57:40 +0300

    refactored + add new functions

--- crud/timetable_user.py
+++ crud/timetable_user.py
@@ -2,13 +2,12 @@ from sqlalchemy.orm import Session
 from sqlalchemy import Column, Integer, func, exists, and_
 
 from sql import models
-from models import schemas
+from models.schemas import TimetableUserCreate, TimetableUserStatuses
 
 
 def create_timetable_user_relation(
-    db: Session, 
-    timetable_user_relation: schemas.TimetableUserCreate
-):
+        db: Session, 
+        timetable_user_relation: TimetableUserCreate):
     """Привязывает пользователя и расписание"""
     db_timetable_user_relation = models.TimetableUser(
         id_user = timetable_user_relation.id_user,
@@ -20,70 +19,69 @@ def create_timetable_user_relation(
 
 
 def get_timetable_user_status(
-    db: Session, 
-    user_id: int | Column[Integer], 
-    timetable_id: int | Column[Integer]
-) -> schemas.TimetableUserStatuses | None:
+        db: Session, user_id: int | Column[Integer], 
+        timetable_id: int | Column[Integer]) -> TimetableUserStatuses | None:
     """Отдаёт статус пользователя в расписании"""
-    status = db.query(models.TimetableUser.status).filter(
+    return db.query(models.TimetableUser.status).filter(
         models.TimetableUser.id_user == user_id,
-        models.TimetableUser.id_timetable == timetable_id,
-        ).first()
-    return status[0] if status else None
+        models.TimetableUser.id_timetable == timetable_id).scalar()
 
 
-def get_timetable_elder(
-    db: Session, 
-    timetable_id: int | Column[Integer]
-) -> int | None:
-    """Отдаёт id пользователя, который в данном расписании является старостой"""
-    user_id =  db.query(models.TimetableUser.id_user).filter(
-        models.TimetableUser.id_timetable == timetable_id,
-        models.TimetableUser.status == schemas.TimetableUserStatuses.elder
-    ).first()
-    return user_id[0] if user_id else None
+def get_user_timetables_id_by_status(
+        db: Session, user_id: int | Column[Integer], 
+        status: TimetableUserStatuses) -> list[int]:
+    """Отдаёт расписания пользователя с данным статусом"""
+    timetables_id = db.query(models.TimetableUser.id_timetable).filter(
+        models.TimetableUser.id_user == user_id,
+        models.TimetableUser.status == status).all()
+    return [id[0] for id in timetables_id]
 
 
 def get_amount_of_user_timetables(
-    db: Session, 
-    user_id: int | Column[Integer]
-) -> int:
+        db: Session, user_id: int | Column[Integer]) -> int:
     """Отдаёт количество расписаний пользователя"""
     return db.query(func.count(models.TimetableUser.id_timetable)).filter(
         models.TimetableUser.id_user == user_id).scalar()
 
 
-def exists_timetable_user_relation(
-    db: Session, 
-    user_id: int | Column[Integer], 
-    timetable_id: int | Column[Integer]
-) -> bool:
-    """Проверяет, есть ли у данного пользователя данное расписание"""
-    return db.query(exists().where(and_(
-        models.TimetableUser.id_user == user_id,
-        models.TimetableUser.id_timetable == timetable_id
-    ))).scalar()
-
-
 def get_timetable_users_id(
-    db: Session, 
-    timetable_id: int | Column[Integer]
-) -> list[int]:
+        db: Session, timetable_id: int | Column[Integer]) -> list[int]:
     """Отдаёт id всех пользователей, прикрепленных к данному расписанию"""
     users_id = db.query(models.TimetableUser.id_user).filter(
-        models.TimetableUser.id_timetable == timetable_id
-    ).all()
+        models.TimetableUser.id_timetable == timetable_id).all()
     return [user_id[0] for user_id in users_id]
 
 
+def exists_timetable_user_relation(
+        db: Session, user_id: int | Column[Integer], 
+        timetable_id: int | Column[Integer]) -> bool:
+    """Проверяет, есть ли у данного пользователя данное расписание"""
+    return db.query(exists().where(and_(
+        models.TimetableUser.id_user == user_id,
+        models.TimetableUser.id_timetable == timetable_id))).scalar()
+
+
+def have_user_enough_rights_in_timetable(
+        db: Session, user_id: int | Column[Integer], 
+        timetable_id: int | Column[Integer],
+        statuses: list[TimetableUserStatuses] = [TimetableUserStatuses.elder]
+    ) -> bool:
+    """Проверяет, есть ли у данного пользователя достаточно прав в данном
+    расписании (т.е. проверяет статус пользователя в этом расписании). 
+    Автоматически проверяется, является ли пользователь старостой
+    в данном расписании. Можно изменить проверку статуса пользователя
+    в расписании на другой или другие, если их несколько"""
+    return db.query(exists().where(and_(
+        models.TimetableUser.id_user == user_id,
+        models.TimetableUser.id_timetable == timetable_id,
+        models.TimetableUser.status.in_(statuses)))).scalar()
+
+
 def delete_timetable_user_relation(
-    db: Session, 
-    user_id: int | Column[Integer], 
-    timetable_id: int | Column[Integer]
-):
+        db: Session, user_id: int | Column[Integer], 
+        timetable_id: int | Column[Integer]):
     """Открепляет пользователя от расписания"""
     db.query(models.TimetableUser).filter(
         models.TimetableUser.id_user == user_id,
-        models.TimetableUser.id_timetable == timetable_id
-    ).delete()
+        models.TimetableUser.id_timetable == timetable_id).delete()
     db.commit()

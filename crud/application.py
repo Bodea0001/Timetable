054cd7c2caeb1b7b7b729b68c5d5b8667f89b647
Bogdan Objilean <bodea0001@gmail.com>
2023-07-21 16:43:55 +0300

    refactored

--- crud/application.py
+++ crud/application.py
@@ -5,24 +5,18 @@ from sql import models
 
 
 def create_application(
-    db: Session, 
-    user_id: int| Column[Integer], 
-    timetable_id: int | Column[Integer]
-):
+        db: Session, user_id: int| Column[Integer], 
+        timetable_id: int | Column[Integer]):
     """Создаёт заявку на добавление к расписанию"""
     application = models.Application(
-        id_user = user_id,
-        id_timetable = timetable_id
-    )
+        id_user=user_id, id_timetable=timetable_id)
     db.add(application)
     db.commit()
 
 
 def have_user_application(
-    db: Session,
-    user_id: int | Column[Integer],
-    application_id: int | Column[Integer]
-) -> bool:
+        db: Session, user_id: int | Column[Integer],
+        application_id: int | Column[Integer]) -> bool:
     """Проверяет, подавал ли пользователь данную заявку"""
     return db.query(exists().where(and_(
         models.Application.id == application_id,
@@ -30,28 +24,25 @@ def have_user_application(
 
 
 def get_application_by_id(
-    db: Session, 
-    application_id: int | Column[Integer]
-) -> models.Application | None:
+        db: Session, application_id: int | Column[Integer]
+    ) -> models.Application | None:
     """Ищет заявку в БД по ID и отдаёт"""
     return db.query(models.Application).filter(
-        models.Application.id ==application_id).first()
+        models.Application.id == application_id).first()
 
 
-def get_applications_by_timetable_id(
-    db: Session, 
-    timetable_id: int | Column[Integer]
-) -> list[models.Application] | list[None]:
-    """Ищет заявки в БД по ID расписания, к которой 
+def get_applications_by_list_with_timetables_id(
+        db: Session, timetables_id: list[int] | list[Column[Integer]] | None
+    ) -> list[models.Application]:
+    """Ищет заявки в БД по ID расписаний, к которым
     эти заявки относятся, и отдаёт"""
     return db.query(models.Application).filter(
-        models.Application.id_timetable == timetable_id).all()
+        models.Application.id_timetable.in_(timetables_id)).all()
 
 
 def exists_application_with_user_id_and_timetable_id(
-    db: Session, user_id: int | Column[Integer],
-    timetable_id: int | Column[Integer]
-) -> bool:
+        db: Session, user_id: int | Column[Integer],
+        timetable_id: int | Column[Integer]) -> bool:
     """Проверяет, подавал ли данный пользователь заявку на 
     добавление к данному расписанию"""
     return db.query(exists().where(and_(

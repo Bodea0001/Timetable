a261c9adf8487c27848edf3449094013a55b33b9
Bogdan Objilean <bodea0001@gmail.com>
2023-08-08 14:20:47 +0300

    add exception handler

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
 
 from routers import (
     login,
@@ -15,6 +18,14 @@ from routers import (
 
 
 app = FastAPI()
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content=jsonable_encoder({"detail": exc.errors()})
+    )
 
 
 app.include_router(login.router)

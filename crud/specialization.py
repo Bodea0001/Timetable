817cb6a0ae88843fd73843e1818cc8484af7a578
Bogdan Objilean <bodea0001@gmail.com>
2023-08-08 14:19:11 +0300

    add getting id instead of models

--- crud/specialization.py
+++ crud/specialization.py
@@ -5,30 +5,27 @@ from sql import models
 from models import schemas
 
 
-def get_specialization_by_name(
-    db: Session,
-    specialization_name: str | Column[String],
-    education_level: schemas.Education_level
-) -> models.Specialization | None:
+def get_specialization_id_by_name(
+        db: Session,
+        specialization_name: str | Column[String],
+        education_level: schemas.Education_level) -> int | None:
     """Ищет специальность в БД по названию и уровню образования и отдаёт"""
-    return db.query(models.Specialization).filter(
+    return db.query(models.Specialization.id).filter(
         models.Specialization.name == specialization_name,
-        models.Specialization.education_level == education_level).first()
+        models.Specialization.education_level == education_level).scalar()
 
 
-def get_specialization_by_code(
-    db: Session,
-    specialization_code: str | Column[String],
-) -> models.Specialization | None:
+def get_specialization_id_by_code(
+        db: Session, specialization_code: str | Column[String],
+    ) -> models.Specialization | None:
     """Ищет специальность в БД по коду и отдаёт"""
-    return db.query(models.Specialization).filter(
-        models.Specialization.code == specialization_code,).first()
+    return db.query(models.Specialization.id).filter(
+        models.Specialization.code == specialization_code,).scalar()
 
 
 def get_specialization_by_id(
-    db: Session, 
-    specialization_id: int | Column[Integer]
-) -> models.Specialization | None:
+        db: Session, specialization_id: int | Column[Integer]
+    ) -> models.Specialization | None:
     """Ищет специальность в БД по ID и отдаёт"""
     return db.query(models.Specialization).filter(
         models.Specialization.id == specialization_id).first()
--- crud/university.py
+++ crud/university.py
@@ -4,19 +4,17 @@ from sqlalchemy import Column, Integer, String
 from sql import models
 
 
-def get_university(
-    db: Session, 
-    university_name: str | Column[String]
-) -> models.University | None:
+def get_university_id_by_name_from_db(
+        db: Session, university_name: str | Column[String]
+    ) -> int | None:
     """Ищет университет в БД по его имени и отдаёт"""
-    return db.query(models.University).filter(
-        models.University.name == university_name).first()
+    return db.query(models.University.id).filter(
+        models.University.name == university_name).scalar()
 
 
-def get_university_by_id(
-    db: Session,
-    university_id: int | Column[Integer]
-) -> models.University | None:
+def get_university_by_id_from_db(
+        db: Session, university_id: int | Column[Integer]
+    ) -> models.University | None:
     """Ищет университет в БД по его id и отдаёт"""
     return db.query(models.University).filter(
         models.University.id == university_id).first()
